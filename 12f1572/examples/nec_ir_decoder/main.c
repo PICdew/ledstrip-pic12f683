d29e82c787d13ae898ec6b34031d5c9ec844fe9b
pit <samstagssession@gmail.com>
2015-03-08 01:36:13 +0100

    ir nec decoder works

--- 12f1572/examples/nec_ir_decoder/main.c
+++ 12f1572/examples/nec_ir_decoder/main.c
@@ -1,17 +1,36 @@
+/*
+todo
+  repeat code
+  timeout is 65535us (t1 ovf). Detect timing violations asap.
+  detect timing violations on all edges
+  ir_code_bit++ one time is enough
+  ir_code_bit -> ir_code_bit_num
+*/
+
 #include <pic12f1572.h>
 #include <stdint.h>
 
 static __code uint16_t __at (_CONFIG1) configword1 = _FOSC_INTOSC & _WDTE_OFF & _MCLRE_ON & _CP_OFF & _PWRTE_OFF & _BOREN_OFF & _CLKOUTEN_OFF;
 static __code uint16_t __at (_CONFIG2) configword2 = _WRT_OFF & _PLLEN_ON & _STVREN_ON & _DEBUG_OFF & _LVP_ON;
 
-uint8_t  result_width_h = 0;
-uint8_t  result_width_l = 0;
-uint16_t resultier;
-uint8_t  loop;
-uint8_t  nec_first=0;
-uint8_t  state=0;
-uint32_t ir_code=0;
-uint8_t  ir_code_bit=0;
+volatile uint8_t  result_width_h      = 0;
+volatile uint8_t  result_width_l      = 0;
+volatile uint16_t resultier;
+uint8_t           loop;
+volatile uint8_t  state               = 0;
+uint8_t           ir_code_bit         = 255;
+
+typedef union {
+  struct {
+    uint8_t addr  : 8;
+    uint8_t iaddr : 8;
+    uint8_t cmd   : 8;
+    uint8_t icmd  : 8;
+  };
+  uint32_t  b32;
+}ir_code_t;
+
+ir_code_t ir_code;
 
 void init() {
   INTCON  = 0;    // Clear interrupt flag bits
@@ -19,6 +38,8 @@ void init() {
   IOCIE   = 1;    // interrupt on change enable
   IOCAN2  = 1;    // irq on negative edge (RA2/pin5)
   //IOCAP2  = 1;    // irq on positive edge (RA2/pin5)
+  PEIE    = 1;    // peripheral interrupt enable
+  TMR1IE  = 1;    // irq on t1 overflow
 
   // switch off analog
   ANSELA  = 0;
@@ -33,7 +54,7 @@ void init() {
   IRCF1   = 1;    // "
   IRCF0   = 0;    // "
 
-  TRISA5  = 0;    // output for red led     - pin2
+  TRISA5  = 0;    // output for led / scope - pin2
   TRISA2  = 1;    // input  for ir receiver - pin5
 
   TMR1CS0 = 0;    // 8MHz (Fosc/4 -> 32MHz/4 ; timer 1 use internal clock)
@@ -45,62 +66,75 @@ void init() {
   TMR1ON  = 1;    // start to count
 }
 
+void debug() { // just a pulse
+  for(loop=0;loop<100;loop++) {
+    LATA5 = 1;
+  }
+  LATA5 = 0;
+}
+
 void isr (void) __interrupt (1){
   GIE = 0;
-  if(IOCIF){                // IOC?
+
+  if(IOCAF2) { // interrupt on change on pin RA2 occured? (falling edge only / IOCAN2  = 1)
     result_width_h = TMR1H;
     result_width_l = TMR1L;
     TMR1H          = 0;     // reset timer 1
     TMR1L          = 0;     // "
+    IOCAF2         = 0;     // reset irq flag
   }
-  IOCAF = 0;
+
+  if(TMR1IF) { // timer1 ovf - illegal timing found
+    state  = 0;
+    ir_code_bit = 255;
+    TMR1IF = 0; // reset irq flag
+  }
   GIE = 1;
 }
 
+
 void main() {
+  ir_code.b32 = 0;
   init();
 
   while(1){
-// verified timings:
-// header 13000 - 14000
-// zero    1000 -  1200
-// one     2200 -  2300
-//    if(result_width_h > 20 && result_width_h < 255){
-    resultier=result_width_h <<8 | result_width_l;
-    if(resultier > 13000 && resultier < 14000){ // header state = 0b1
-      state = 0b1;
-    }
-    if(state == 0b1 && resultier > 1000 && resultier < 2300){ // bitfields state = 0b10
-      state << 1;
-      if(resultier < 1200){ // zero found
-        ir_code << 1;
-        ir_code_bit++;
-      }
-      if(resultier > 2200){ // one found
-        ir_code << 1;
-        ir_code |= 0x1;
-        ir_code_bit++;
-      }
-    }else{
-      state = 0;
-    }
-    if(state == 0b10 && ir_code_bit == 31){ // trailer state = 0b100
-      state << 1;
-      if(resultier > 1000 && resultier < 1200){
-        state << 1; // successfully decoded -> state = 0b1000
-      }
-    }
+  /*
+    measured timings:
+    header 13000 - 14000
+    zero    1000 -  1200
+    one     2200 -  2300
+  */
 
-// debug only
-    if (state == 0b10 && ir_code_bit == 1){
-      if(ir_code & 0b1){
-        for(loop=0;loop<100;loop++) {
-          LATA5 = 1;// just debugging
+    resultier = result_width_h << 8 | result_width_l;
+    if(state == 0 && resultier > 13000 && resultier < 14000){ // state = 0 means headerfield
+      state = 1; // set state to bitfield
+    }
+    if(state == 1 && resultier > 1000 && resultier < 2300){ // state = 1 means bitfield
+      if(resultier < 1200){ // "0" found
+        ir_code.b32 >>= 1;  // 3h of brain aking - it was ir_code.b32 >> 1 before ;-)
+        ir_code_bit++;
+      }
+      if(resultier > 2200){ // "1" found
+        ir_code.b32 >>= 1;
+        ir_code.b32 |= 0x80000000;
+        ir_code_bit++;
+      }
+      result_width_h = 0; // avoid multiple matches / don't use old measures
+      result_width_l = 0; // "
+
+      if(ir_code_bit == 31) { // last bit decoded / state = 2 means trailerfield
+        state = 2;
+      }
+    }
+    if(state == 2 ) { // decoding complete
+      state = 0; // start over
+      ir_code_bit = 255;
+
+      if( (ir_code.addr ^ ir_code.iaddr == 0xFF) && (ir_code.cmd ^ ir_code.icmd == 0xFF) ) { // error correction
+        if(ir_code.addr == 0){ // my original led strip remote uses addr=0
+          // some application
+          debug();
         }
-        LATA5 = 0;// just debugging
-        result_width_h = 0;
-        result_width_l = 0;
-        state = 0;
       }
     }
   }
